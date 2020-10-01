f78745e890e0528da8a75f4cb87dfa0b03fe7dbd
Shubham Giri <53148932+iamshubhamg@users.noreply.github.com>
2020-10-01 09:48:59 +0530

    Create sudoko.java

---
+++ sudoko.java
@@ -0,0 +1,21 @@
+// A Java program to demonstrate working of 
+// recursion 
+class GFG { 
+	static void printFun(int test) 
+	{ 
+		if (test < 1) 
+			return; 
+		else { 
+			System.out.printf("%d ", test); 
+			printFun(test - 1); // statement 2 
+			System.out.printf("%d ", test); 
+			return; 
+		} 
+	} 
+
+	public static void main(String[] args) 
+	{ 
+		int test = 3; 
+		printFun(test); 
+	} 
+} 
